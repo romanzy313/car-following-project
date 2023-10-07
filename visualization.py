65dd82d0509cfee8baf6f7cc442a9d554b9c2637
Roman <13500086+romanzy313@users.noreply.github.com>
2023-10-07 20:02:54 +0200

    added read_data from supervisor

--- visualization.py
+++ visualization.py
@@ -1,14 +1,38 @@
 import pygame
-from src.load_data import load_data
+from src.read_data import read_data
+
+
+class Vehicle:
+    x: float
+    v: float
+    a: float
+
+    def __init__(self):
+        # body of the constructor
+        return
+
+    def tick(self, dt, newAcceleration):
+        self.a = newAcceleration
+        self.v = self.v + newAcceleration * dt
+        self.x = self.x + self.v * dt
+
+        return
 
 
 def main():
     # load zaar data
-    load_data("data/trainAH.zarr")
+    data = read_data(cfpair="AH", dataset="train")
+
+    print("data is", data)
+
+    screen_width = 1280
+    screen_height = 720
+
+    road_radius = 720 / 2 - 50
 
     # pygame setup
     pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
+    screen = pygame.display.set_mode((screen_width, screen_height))
     clock = pygame.time.Clock()
     running = True
     dt = 0
@@ -16,6 +40,9 @@ def main():
     player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 
     while running:
+        # screen =
+        center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
@@ -26,6 +53,8 @@ def main():
         screen.fill("purple")
 
         pygame.draw.circle(screen, "red", player_pos, 40)
+
+        pygame.draw.circle(screen, "blue", center, road_radius, 20)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
