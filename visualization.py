32e57d7116fbc06df9897b0e5a9b3d3e2b1630b0
Roman <13500086+romanzy313@users.noreply.github.com>
2023-10-23 02:17:09 +0200

    proper vehicle visualization

--- visualization.py
+++ visualization.py
@@ -1,48 +1,59 @@
+# import math
+import math
 import pygame
-from src.read_data import read_data
+from visualization.car import Car
+from visualization.constants import *
 
 
-class Vehicle:
-    x: float
-    v: float
-    a: float
+def rot_center(image, angle, x, y):
+    rotated_image = pygame.transform.rotate(image, angle)
+    new_rect = rotated_image.get_rect(center=image.get_rect(center=(x, y)).center)
+    return rotated_image, new_rect
 
-    def __init__(self):
-        # body of the constructor
-        return
 
-    def tick(self, dt, newAcceleration):
-        self.a = newAcceleration
-        self.v = self.v + newAcceleration * dt
-        self.x = self.x + self.v * dt
+def blitRotate(surf, image, pos, originPos, angle):
+    # offset from pivot to center
+    image_rect = image.get_rect(topleft=(pos[0] - originPos[0], pos[1] - originPos[1]))
+    offset_center_to_pivot = pygame.math.Vector2(pos) - image_rect.center
 
-        return
+    # roatated offset from pivot to center
+    rotated_offset = offset_center_to_pivot.rotate(-angle)
+
+    # roatetd image center
+    rotated_image_center = (pos[0] - rotated_offset.x, pos[1] - rotated_offset.y)
+
+    # get a rotated image
+    rotated_image = pygame.transform.rotate(image, angle)
+    rotated_image_rect = rotated_image.get_rect(center=rotated_image_center)
+
+    # rotate and blit the image
+    surf.blit(rotated_image, rotated_image_rect)
+
+    # draw rectangle around the image
+    pygame.draw.rect(
+        surf, (255, 0, 0), (*rotated_image_rect.topleft, *rotated_image.get_size()), 2
+    )
+
+
+def drawVehicle(surf, image, angle):
+    pass
 
 
 def main():
-    # load zaar data
-    data = read_data(cfpair="AH", dataset="train")
-
-    print("data is", data)
-
-    screen_width = 1280
-    screen_height = 720
-
-    road_radius = 720 / 2 - 50
-
     # pygame setup
     pygame.init()
-    screen = pygame.display.set_mode((screen_width, screen_height))
+
+    screen = pygame.display.set_mode(display_size)
+    pygame.display.set_caption("Car follower visualization")
     clock = pygame.time.Clock()
     running = True
     dt = 0
+    angle = 0
+    speed = 100
 
-    player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    w, h = car_image.get_size()
 
     while running:
-        # screen =
-        center = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
         # poll for events
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
@@ -50,21 +61,39 @@ def main():
                 running = False
 
         # fill the screen with a color to wipe away anything from last frame
-        screen.fill("purple")
+        screen.fill(display_color)
+        pygame.draw.rect(screen, vis_color, vis_rect)
+        pygame.draw.circle(screen, road_color, vis_center, road_radius, road_width)
 
-        pygame.draw.circle(screen, "red", player_pos, 40)
+        # Calculate the new position
+        x = vis_center[0] + (road_radius - road_width / 2) * math.sin(
+            math.radians(angle)
+        )
+        y = vis_center[1] + (road_radius - road_width / 2) * math.cos(
+            math.radians(angle)
+        )
 
-        pygame.draw.circle(screen, "blue", center, road_radius, 20)
+        # x = vis_center[0] + 30
+        # y = vis_center[1]
+
+        blitRotate(screen, car_image, (x, y), (w / 2, h / 2), angle - 90)
+
+        # screen.blit(rotate2d(car_image, (x,y), angle), (x,y))
+
+        # rotate is not proper
+        # screen.blit(
+        #     pygame.transform.rotate(car_image, -angle),
+        #     (x + car_image_rect.centerx / 2, y + car_image_rect.centery / 2),
+        # )
+        # pygame.draw.circle(screen, "red", player_pos, 40)
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_w]:
-            player_pos.y -= 300 * dt
+            speed += 100 * dt
         if keys[pygame.K_s]:
-            player_pos.y += 300 * dt
-        if keys[pygame.K_a]:
-            player_pos.x -= 300 * dt
-        if keys[pygame.K_d]:
-            player_pos.x += 300 * dt
+            speed -= 100 * dt
+
+        angle += speed * dt
 
         # flip() the display to put your work on screen
         pygame.display.flip()
--- visualization/constants.py
+++ visualization/constants.py
@@ -7,7 +7,7 @@ vis_color = (30, 30, 30)
 vis_center = (vis_rect[2] / 2, vis_rect[3] / 2)
 
 road_radius = 300
-road_width = 30
+road_width = 40
 road_color = (255, 255, 255)
 
 car_image = pygame.image.load("assets/car_1.png")
