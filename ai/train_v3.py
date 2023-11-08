72bfdfc0946bfd1784519cb26fb1651f55b749fc
RancherTom <bjwzc2000@gmail.com>
2023-11-08 01:35:30 +0100

    Successful train_v3

--- ai/train_v3.py
+++ ai/train_v3.py
@@ -2,10 +2,8 @@
 import numpy as np
 from pandas import DataFrame
 from Sec2SecRuntime import Seq2Seq
-from read_data import read_clustered_data
 import multiprocessing
 import os
-from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 import torch
@@ -18,14 +16,13 @@ from torch.utils.data import DataLoader, TensorDataset, Dataset
 from glob import glob
 import re
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-from Split_dataloader import create_dataloader
+from Split_dataloader import prepare_dataloaders
 
 
 def train_model(
     model,
     train_dataloader,
     val_dataloader,
-    scaler,
     epochs,
     optimizer,
     loss_function,
@@ -53,7 +50,7 @@ def train_model(
 
             output_seq = model(input_seq)
 
-            loss = loss_function(output_seq, ground_truth)
+            loss = loss_function(output_seq[:, :, 0], ground_truth)
             loss.backward()
             optimizer.step()
 
@@ -69,11 +66,13 @@ def train_model(
 
                 output_seq = model(input_seq)
 
-                val_loss += loss_function(output_seq, ground_truth).item()
+                val_loss += loss_function(output_seq[:, :, 0], ground_truth).item()
 
                 # The following normalization and MSE calculation can be refactored into a function
                 # to reduce redundancy between training and validation loops.
-                mse_error = calculate_denormalized_mse(output_seq, ground_truth, scaler)
+                mse_error = torch.nn.functional.mse_loss(
+                    output_seq[:, :, 0], ground_truth
+                )
 
                 # Calculate MSE error and append to list
                 mse_errors.append(mse_error)
@@ -97,14 +96,13 @@ def train_model(
 
         # Print epoch stats
         print(
-            f"[{dataset}_{cluster_idx}] Epoch: {epoch} Loss: {loss.item():.4f} Val Loss: {val_loss:.4f} Avg MSE: {avg_mse_error:.4f}"
+            f"[{dataset}_{cluster_idx}] Epoch: {epoch} Loss: {loss.item():.4f} Val Loss: {val_loss:.4f} Avg MSE: {avg_mse_error:.4f}"  # type: ignore
         )
 
 
 def run_training(
     train_dataloader,
     eval_dataloader,
-    scaler,
     dataset,
     cluster_idx,
     n_steps_out,
@@ -141,7 +139,6 @@ def run_training(
         model,
         train_dataloader,  # Pass the DataLoader instead of tensors directly
         eval_dataloader,
-        scaler,
         epochs,
         optimizer,
         loss_function,
@@ -149,44 +146,11 @@ def run_training(
     )
 
     file_location = f"{brain_dir}/{dataset}_{cluster_idx}.pth"
-    # Save the model and scaler for this cluster
+    # Save the model for this cluster
     torch.save(
-        {"model_state_dict": model.state_dict(), "scaler": scaler},
+        {"model_state_dict": model.state_dict()},
         file_location,
     )
-
-    # evaluate_model(dataset, cluster_idx, model, scaler, device, eval_dataloader)
-    # type: ignore
-    models_scalers = (model, scaler)
-
-    return models_scalers
-
-
-def calculate_denormalized_mse(output, target, scaler):
-    """
-    Calculate the Mean Squared Error (MSE) between the model's outputs and the ground truth
-    after denormalizing the data.
-
-    Parameters:
-    - output: the output tensor from the model
-    - target: the ground truth tensor
-    - scaler: the scaler instance used for normalizing/denormalizing the data
-
-    Returns:
-    - mse: the calculated mean squared error after denormalization
-    """
-    # Move tensors to CPU and convert to numpy for sklearn compatibility
-    output_np = output.cpu().numpy()
-    target_np = target.cpu().numpy()
-
-    # Denormalize the data
-    output_denorm = scaler.inverse_transform(output_np.reshape(-1, output_np.shape[-1]))
-    target_denorm = scaler.inverse_transform(target_np.reshape(-1, target_np.shape[-1]))
-
-    # Calculate MSE
-    mse = mean_squared_error(target_denorm, output_denorm)
-
-    return mse
 
 
 def find_all_clusters():
@@ -207,44 +171,16 @@ def find_all_clusters():
     # for i in tqdm(datasets, position=0, leave=False, desc="i", colour="green"):
 
 
-def preprocess_data(df):
-    scaler = StandardScaler()
-
-    data_normalized = scaler.fit_transform(df)
-    train_df, val_df = train_test_split(data_normalized, test_size=0.2, random_state=42)
-    train_df = DataFrame(train_df)
-    val_df = DataFrame(val_df)
-    return (train_df, val_df, scaler)
-
-
 def train_cluster(dataset: str, cluster_idx: int, train_dataloader, eval_dataloader):
-    # train_data = read_clustered_data(file)
-    # train_df, val_df, scaler = preprocess_data(train_data)
-
-    # train_seq = create_sequences(train_df)
-    # val_seq = create_sequences(val_df)
-
-    # train_seq = MyCustomDataset(train_seq)
-    # val_seq = MyCustomDataset(val_seq)
-
-    # # sequenced_dataset = MyCustomDataset(data_points)
-    # print(type(train_seq))
-    # print(f"This is the shape after sequenced", train_seq.__len__())
-
-    # tqdm.write(f"[{dataset}_{cluster_idx}] Dataset size {train_seq.__len__()}")
-
-    # train_data = train_data.sample(frac=0.01, random_state=1)
     run_training(
         train_dataloader=train_dataloader,
         eval_dataloader=eval_dataloader,
-        scaler=scaler,  # type: ignore
         dataset=dataset,
         cluster_idx=cluster_idx,
         n_steps_out=n_steps_out,
         epochs=epochs,
         lr=lr,
         device=device,
-        num_workers=num_workers,
     )
 
 
@@ -261,13 +197,15 @@ num_workers = multiprocessing.cpu_count() / 2
 
 # set the dataset and mode
 if __name__ == "__main__":
-    # datas = find_all_clusters()
-    datas = [{"dataset": "AH", "cluster": 0, "file": "../out_cluster/AH_0.zarr"}]
-    train_dataloader, eval_dataloader = create_dataloader("AH", "train")
+    dataset = "HH"
+    cluster_idx = 0
+    # datas = [{"dataset": "AH", "cluster": 0, "file": "../out_cluster/AH_0.zarr"}]
 
-    os.makedirs(brain_dir, exist_ok=True)
-    for v in tqdm(datas, position=0, leave=False, desc="per cluster", colour="green"):
-        dataset = v["dataset"]
-        cluster_idx = v["cluster"]
-        file = v["file"]
-        train_cluster(dataset, cluster_idx, train_dataloader, eval_dataloader)
+    train_dataloader, eval_dataloader = prepare_dataloaders(dataset, cluster_idx)
+    train_cluster(dataset, cluster_idx, train_dataloader, eval_dataloader)
+    # os.makedirs(brain_dir, exist_ok=True)
+    # for v in tqdm(datas, position=0, leave=False, desc="per cluster", colour="green"):
+    #     dataset = v["dataset"]
+    #     cluster_idx = v["cluster"]
+    #     file = v["file"]
+    #     train_cluster(dataset, cluster_idx, train_dataloader, eval_dataloader)
